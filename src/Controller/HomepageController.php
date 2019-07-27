8eaf8f0d5fba557de781feb4de6acd4ae835af44
fschutt <felix.schuett@maps4print.com>
2019-07-27 15:44:43 +0200

    Fix CSS to make main column narrower

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -13,7 +13,7 @@ class HomepageController extends AbstractController
             (object)[
                 'title' => 'Hallo Welt!',
                 'link' => '2019-07-25/hallo-welt-1',
-                'body' => 'Lorem ipsum dolor sit amet',
+                'body' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             ],
             (object)[
                 'title' => 'Hallo Welt!',
