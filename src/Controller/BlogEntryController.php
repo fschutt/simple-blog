83f27d69fb35f5b8c193843de13ca4ae21d8c315
fschutt <felix.schuett@maps4print.com>
2019-07-27 16:11:33 +0200

    Match "/" tokens in URL to be blogpost links

--- src/Controller/BlogEntryController.php
+++ src/Controller/BlogEntryController.php
@@ -9,6 +9,9 @@ class BlogEntryController extends AbstractController
 {
     public function html($slug)
     {
+        // TODO: lookup $slug in blogposts
+        // if not found, redirect to 404 page
+        // if found
         return $this->render('blogeintrag.html.twig', [
             'controller_name' => 'BlogEntryController',
             'slug' => $slug,
