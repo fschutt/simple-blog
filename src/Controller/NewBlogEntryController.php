5eff6cd89f3c2ac10881f400b9508583ec9d2860
fschutt <felix.schuett@maps4print.com>
2019-08-03 16:29:24 +0200

    Added stub for SQL queries in Controller

--- src/Controller/NewBlogEntryController.php
+++ src/Controller/NewBlogEntryController.php
@@ -9,6 +9,35 @@ class NewBlogEntryController extends AbstractController
 {
     public function html()
     {
+        /*
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form = $this->createFormBuilder()
+        ->add('phone', 'text', array('attr' => array('autofocus' => '')))
+        ->add('period', 'number', array('attr' => array('value' => '12')))
+        ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            // data is an array with "phone" and "period" keys
+            $data = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+
+            $contract = $em->getRepository('FrontBundle:Contract')->getContractByPhone($data["phone"]);
+            $contract->setOwner("John Doe");
+            $contract->setPhone($data["phone"]);
+
+            // or this could be $contract = new Contract("John Doe", $data["phone"], $data["period"]);
+
+
+            $em->persist($contract);
+        }
+
+        */
         return $this->render('neuer-eintrag.html.twig', [
 
         ]);
